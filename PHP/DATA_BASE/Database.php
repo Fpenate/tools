fb5b0effff8815c2731400cab5f6d5b19aa25796
Fpenate <64710252+Fpenate@users.noreply.github.com>
2025-04-02 11:23:49 -0600

    Create Database.php

---
+++ PHP/DATA_BASE/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database {
+    private $pdo;
+
+    public function __construct() {
+        $config = require 'config.php';
+        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        try {
+            $this->pdo = new PDO($dsn, $config['user'], $config['password'], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
+
+?>
