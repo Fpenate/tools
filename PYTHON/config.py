f02708d88c33aefcfff354a745ad639ab170209e
Fpenate <64710252+Fpenate@users.noreply.github.com>
2025-04-02 11:38:29 -0600

    Delete PYTHON/config.py

--- PYTHON/config.py
+++
@@ -1,9 +0,0 @@
-# Configuración de la base de datos
-DB_CONFIG = {
-    "connection": "mysql",  # Cambiar a "pgsql" si se usa PostgreSQL
-    "host": "localhost",
-    "dbname": "mi_base_de_datos",
-    "user": "mi_usuario",
-    "password": "mi_contraseña",
-    "charset": "utf8mb4"
-}
