9383c4c40278bb47ae57bf6ba5a0b51fc3c1298d
Fpenate <64710252+Fpenate@users.noreply.github.com>
2025-04-02 14:35:14 -0600

    Update config.php

--- PHP/LOGIN/config.php
+++ PHP/LOGIN/config.php
@@ -1,5 +1,6 @@
 <?php
 // Configuración de usuarios permitidos (para pruebas, en un sistema real usar una base de datos)
+//Este archivo contiene los datos de acceso de los usuarios (en una base de datos)
 return [
     'users' => [
         'admin' => '123456', // Usuario: admin, Contraseña: 123456
